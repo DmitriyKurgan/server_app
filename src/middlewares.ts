3855b2de8d914a82894e9966d32b605ffb1b83c9
Dmitry Kurgan <dimakurgan123789@gmail.com>
2024-01-22 19:24:51 +0200

    changes

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,20 +1,16 @@
 const { body, validationResult } = require('express-validator');
 
-// Middleware для валидации тела запроса
 const validateRequest = [
     body('title').isString().isLength({ max: 40 }),
     body('author').isString().isLength({ max: 20 }),
-    body('availableResolutions').isArray().notEmpty().custom((value:number) => {
+    body('availableResolutions').isArray().notEmpty().custom((value, { req }) => {
         if (!Array.isArray(value) || value.length === 0) {
             throw new Error('At least one resolution should be added');
         }
 
-        // Проверка, что все значения в массиве являются строками
         if (!value.every((resolution) => typeof resolution === 'string')) {
             throw new Error('All resolutions must be strings');
         }
-
-        // Проверка, что значения соответствуют разрешенным значениям
         const allowedResolutions = ['P144', 'P240', 'P360', 'P480', 'P720', 'P1080', 'P1440', 'P2160'];
         if (!value.every((resolution) => allowedResolutions.includes(resolution))) {
             throw new Error('Invalid resolution value');
@@ -22,4 +18,16 @@ const validateRequest = [
 
         return true;
     }),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const errorMessages = errors.array().map(error => ({
+                message: error.msg,
+                field: error.param
+            }));
+            return res.status(400).json({ errorsMessages: errorMessages });
+        }
+        next();
+    }
 ];
