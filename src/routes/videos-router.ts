cbe607e290be8ff60c53db46c196a3aff7ce0773
Dmitry Kurgan <dimakurgan123789@gmail.com>
2024-01-28 01:19:48 +0200

    changes

--- src/routes/videos-router.ts
+++ src/routes/videos-router.ts
@@ -128,7 +128,7 @@ videosRouter.put('/:id', validateRequest, (req: Request, res: Response) => {
     videoById.canBeDownloaded = req.body.canBeDownloaded ?? videoById.canBeDownloaded;
     videoById.minAgeRestriction = req.body.minAgeRestriction ?? videoById.minAgeRestriction;
 
-    modifiedDate.setMilliseconds(modifiedDate.getMilliseconds() - 183);
+    modifiedDate.setMilliseconds(modifiedDate.getMilliseconds() - 90);
 
     videoById.publicationDate = modifiedDate.toISOString();
 
